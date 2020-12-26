5a1bd7392bf71d1af053525faa324f67f9cd1f8d
TIAGO NOVAES DIAS <tndias@gmail.com>
2020-12-26 11:49:24 -0300

    Just transform the old site in a react app (just for fun)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="container">
+    <div className="row">
+    <div className="col-xs-12">
+      <div className="jumbotron" data-position="center center">
+        <h1>Tiago Novaes Dias</h1>
+        <hr />
+        <div className="btn-group">
+          <a className="btn btn-default" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/tndias">
+            <i className="fa fa-linkedin" title="Linkedin" />
+          </a>
+          <a className="btn btn-default" target="_blank" rel="noreferrer" href="https://instagram.com/psoabr/">
+            <i className="fa fa-instagram" title="Instagram" />
+          </a>
+          <a className="btn btn-default" target="_blank" rel="noreferrer" href="https://www.facebook.com/tndias">
+            <i className="fa fa-facebook" title="Facebook" />
+          </a>
+          <a className="btn btn-default" target="_blank" rel="noreferrer" href="https://twitter.com/psoa">
+            <i className="fa fa-twitter" title="Twitter" />
+          </a>
+        </div>
+      </div>
     </div>
+  </div>
+  </div>
   );
 }
 
